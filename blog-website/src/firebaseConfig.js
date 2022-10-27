03a08d8037b3acc1039c75fc697453c2cde0a1f4
NchiaEmmanuela <ekeinchiaemmanuela@gmail.com>
2022-10-27 16:27:41 +0100

    connection setup

---
+++ blog-website/src/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCxmlKGJEPrj4vi5qeDast0vkyDU3k7OVU",
+    authDomain: "blog-website-3ded6.firebaseapp.com",
+    projectId: "blog-website-3ded6",
+    storageBucket: "blog-website-3ded6.appspot.com",
+    messagingSenderId: "539037640590",
+    appId: "1:539037640590:web:99e80e5607577819f1a6df"
+  };
+
+  const app = initializeApp(firebaseConfig);
