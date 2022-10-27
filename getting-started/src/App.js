3fe8196519fb30de6743166cd75dbbcdc6b5ffd0
NchiaEmmanuela <ekeinchiaemmanuela@gmail.com>
2022-10-27 13:14:49 +0100

    establish connection to the users collection

--- getting-started/src/App.js
+++ getting-started/src/App.js
@@ -1,6 +1,18 @@
 import './App.css';
+import { useState, useEffect } from 'react'
+import {db} from './firebase-config'
+import { collection } from '@firebase/firestore'
 
 function App() {
+  const [users, setUsers] = useState()
+  const userCollectionRef = collection(db, "users")
+  useEffect(() => {
+    const getUsers = async () => {
+
+    }
+
+    getUsers()
+  }, [])
   return (
     <div className="App">
     </div>
