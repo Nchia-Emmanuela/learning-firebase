7c2bf685c3c8ac4f4a8bcacf5c73b5e832b4c9e8
NchiaEmmanuela <ekeinchiaemmanuela@gmail.com>
2022-10-27 12:36:59 +0100

    firebase setup

---
+++ getting-started/src/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD6ezjUP-PpWlsD79kqT_ngI8VDLjmlB4w",
+    authDomain: "getting-started-decea.firebaseapp.com",
+    projectId: "getting-started-decea",
+    storageBucket: "getting-started-decea.appspot.com",
+    messagingSenderId: "154940309292",
+    appId: "1:154940309292:web:2824ccb114c093aec97689",
+    measurementId: "G-BG3ZZWJQN4"
+  };
