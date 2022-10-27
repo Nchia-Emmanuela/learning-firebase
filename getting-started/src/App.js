f5840eb43723d930fc5882867f515eb6df96011a
NchiaEmmanuela <ekeinchiaemmanuela@gmail.com>
2022-10-27 13:55:32 +0100

    display users

--- getting-started/src/App.js
+++ getting-started/src/App.js
@@ -1,20 +1,32 @@
-import './App.css';
-import { useState, useEffect } from 'react'
-import {db} from './firebase-config'
-import { collection } from '@firebase/firestore'
+import "./App.css";
+import { useState, useEffect } from "react";
+import { db } from "./firebase-config";
+import { collection, getDocs } from "@firebase/firestore";
 
 function App() {
-  const [users, setUsers] = useState()
-  const userCollectionRef = collection(db, "users")
+  const [users, setUsers] = useState();
+  const userCollectionRef = collection(db, "users");
   useEffect(() => {
     const getUsers = async () => {
+      const data = await getDocs(userCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
 
-    }
+    getUsers();
+  }, []);
 
-    getUsers()
-  }, [])
   return (
     <div className="App">
+      {" "}
+      {users?.map((user) => {
+        return (
+          <div>
+            <h1>{user.name}</h1>
+            <h1>{user.age}</h1>
+            <h1>{user.id}</h1>
+          </div>
+        );
+      })}
     </div>
   );
 }
