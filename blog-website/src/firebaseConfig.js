b23584b4b44004c9c6cbf6bcff46776f9eb8650c
NchiaEmmanuela <ekeinchiaemmanuela@gmail.com>
2022-10-27 16:38:52 +0100

    connection to database established

--- blog-website/src/firebaseConfig.js
+++ blog-website/src/firebaseConfig.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: "AIzaSyCxmlKGJEPrj4vi5qeDast0vkyDU3k7OVU",
@@ -10,3 +12,9 @@ const firebaseConfig = {
   };
 
   const app = initializeApp(firebaseConfig);
+
+  export const auth = getAuth(app)
+
+  export const db = getFirestore(app)
+
+  export const provider = new GoogleAuthProvider()
