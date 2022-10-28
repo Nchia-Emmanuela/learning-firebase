d3d827da8e97d5eb5003ecfde716343de25da889
NchiaEmmanuela <ekeinchiaemmanuela@gmail.com>
2022-10-28 11:25:27 +0100

    login with google

--- blog-website/src/pages/Login.jsx
+++ blog-website/src/pages/Login.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { auth, provider } from '../firebaseConfig'
+import { signInWithPopup } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
 
-function Login() {
+function Login( { setIsAuth } ) {
+  const navigate = useNavigate()
+  const handleSignIn = ()=> {
+    signInWithPopup(auth,provider).then((result) => {
+      localStorage.setItem("isAuth", true)
+      setIsAuth(true)
+      navigate("/")
+      
+    })
+  }
   return (
-    <div>Login</div>
+    <div className='login'>
+      <p>Sign In with Google to Continue</p>
+      <button className='btn' onClick={handleSignIn}>Sign In with Google</button>
+    </div>
   )
 }
 
